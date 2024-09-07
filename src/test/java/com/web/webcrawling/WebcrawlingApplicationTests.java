fbf1d55bbbb680da56c53686b3010d8fe40b9842
ssor000r <limsj08@naver.com>
2024-09-07 16:51:45 +0900

    first upload

---
+++ src/test/java/com/web/webcrawling/WebcrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.webcrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
